b28c99fb7d74aa363f0d2bc8a679d48d37309bc8
sunshinesong <8124611+xing0827@user.noreply.gitee.com>
2022-12-04 17:20:40 +0800

    调整目录结构

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,66 +1,22 @@
 <template>
   <div class="custom-element">
-    <button demoBtn @click="onClick1">显示Toast1</button>
-    <button demoBtn @click="onClick2">显示Toast2</button>
+    <toast-demo></toast-demo>
     <br>
-    <button demoBtn @click="Loading(true)">展示默认加载动画</button>
-    <button demoBtn @click="customLoading">展示自定义加载动画</button>
-    <button demoBtn @click="Loading(false)">隐藏加载动画/加载完成</button>
-
+    <loading-demo></loading-demo>
   </div>
 </template>
 
 <script setup>
+import { createApp } from 'vue';
 // toast
-const onClick1 = () => {
-  _that.showToast('我显示了！', 3000)
-}
-const onClick2 = () => {
-  _that.showToast('我显333！', 3000)
-}
+import toastDemo from './exampleVue/toastDemo.vue'
 // loading
-const Loading = (bool) => {
-  if (bool) {
-    _that.showLoading()
-  } else {
-    _that.hideLoading()
-  }
-}
-const customLoading = () => {
-  _that.loadingConfig = `
-    <style>
-      .animation-demo {
-        border-radius: 4px;
-        background-color: rgba(0, 0, 0, .7);
-        height: 44px;
-        width: 44px;
-        display: grid;
-        place-items: center;
-      }
-      .animation-demo > span {
-        height: 19px;
-        width: 19px;
-        border: 1px solid #fff;
-        border-top-color: transparent;
-        border-radius: 19px;
-        animation: rotate infinite forwards .5s linear;
-      }
-      
-      @keyframes rotate {
-        from {
-          transform: rotate(0);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-    </style>
-    <div class="animation-demo">
-      <span></span>
-    </div>
-  `
-  Loading(true)
-}
+import loadingDemo from './exampleVue/loadingDemo.vue'
+
+createApp().component(
+  toastDemo,
+  loadingDemo
+)
 </script>
 
 <style>
---
+++ src/exampleVue/toastDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  <button demoBtn @click="onClick1">显示Toast1</button>
+  <button demoBtn @click="onClick2">显示Toast2</button>
+</template>
+
+<script setup>
+const onClick1 = () => {
+  _that.showToast('我显示了！', 3000)
+}
+const onClick2 = () => {
+  _that.showToast('我显333！', 3000)
+}
+</script>
